2875a7182bf3a778f3a07725e3407edab065711b
Johnsonjrf <johnsonoyetunji@gmail.com>
2023-06-25 21:51:45 +0100

    initial commit for cloned linkedin

---
+++ main.js
@@ -0,0 +1,4 @@
+document.querySelector(".online").addEventListener("click", (e) => {
+    const profileMenu = document.querySelector(".profile-menu-wrap")
+    profileMenu.classList.toggle("show-profile-menu")
+})
